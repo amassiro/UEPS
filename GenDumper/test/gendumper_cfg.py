5f0bb18b9dc90c450c733d24c44edbea02655f68
Andrea <massironi.andrea@gmail.com>
2014-02-10 18:07:20 +0100

    up

--- GenDumper/test/gendumper_cfg.py
+++ GenDumper/test/gendumper_cfg.py
@@ -1,20 +1,28 @@
 import FWCore.ParameterSet.Config as cms
+from FWCore.ParameterSet.VarParsing import VarParsing
 
-process = cms.Process("Demo")
+options = VarParsing ('analysis')
+
+# add a list of strings for events to process
+options.parseArguments()
+
+
+process = cms.Process("SimpleGenDumper")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 10000
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
-    fileNames = cms.untracked.vstring(
-        'file:/tmp/amassiro/WW1Mevents_TUNE_NoUE_2.root'
-    )
+    fileNames = cms.untracked.vstring (options.inputFiles),
+    #fileNames = cms.untracked.vstring('file:/tmp/amassiro/WW1Mevents_TUNE_NoUE_2.root')
 )
 
 process.TFileService = cms.Service("TFileService",
-      fileName = cms.string("/tmp/amassiro/WW1Mevents_TUNE_NoUE_2_dump_tree.root"),
+      #fileName = cms.string("/tmp/amassiro/WW1Mevents_TUNE_NoUE_2_dump_tree.root"),
+      fileName = cms.string (options.outputFile),
       closeFileFast = cms.untracked.bool(True)
 )
 
