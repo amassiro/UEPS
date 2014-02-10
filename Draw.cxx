c4a79e292f94547a20f62c4995d352f5240657eb
Andrea <massironi.andrea@gmail.com>
2014-02-10 18:51:49 +0100

    Draw

---
+++ Draw.cxx
@@ -0,0 +1,104 @@
+{
+#include <algorithm>
+
+ TFile* fCMS   = new TFile ("/tmp/amassiro/WW1Mevents_TUNE_CMS_dump_tree.root");
+ TFile* fATLAS = new TFile ("/tmp/amassiro/WW1Mevents_TUNE_ATLAS_dump_tree.root");
+ TFile* fNoUE  = new TFile ("/tmp/amassiro/WW1Mevents_TUNE_NoUE_dump_tree.root");
+
+ TTree* tCMS   =  (TTree*) fCMS   -> Get ("Analyzer/myTree");
+ TTree* tATLAS =  (TTree*) fATLAS -> Get ("Analyzer/myTree");
+ TTree* tNoUE  =  (TTree*) fNoUE  -> Get ("Analyzer/myTree");
+
+ Double_t X[200];
+ Double_t Y_e0_CMS[200];
+ Double_t Y_e0_ATLAS[200];
+ Double_t Y_e0_NoUE[200];
+ Double_t Y_Delta_e0_up[200];
+ Double_t Y_Delta_e0_do[200];
+
+ Double_t Y_Delta_e0_overE_up[200];
+ Double_t Y_Delta_e0_overE_do[200];
+
+ Double_t Y_One[200];
+ Double_t X_Zero[200];
+
+ int n = 3;
+ float totCMS   = tCMS   -> GetEntries();
+ float totATLAS = tATLAS -> GetEntries();
+ float totNoUE  = tNoUE  -> GetEntries();
+
+ for (int i=0; i<n; i++) {
+  double threshold = 10+i*2;
+  std::cout << " n[" << i << "]:: threshold = " << threshold;
+// double threshold = 25+i;
+  X[i] = threshold;
+  TString s1 = Form ("jetpt1>=%f",threshold);
+
+  float temp;
+
+  temp = tCMS   -> GetEntries(s1.Data()); Y_e0_CMS[i]   = temp  / totCMS;
+  temp = tATLAS -> GetEntries(s1.Data()); Y_e0_ATLAS[i] = temp  / totATLAS;
+  temp = tNoUE  -> GetEntries(s1.Data()); Y_e0_NoUE[i]  = temp  / totNoUE;
+
+  float delta[2];
+  delta[0] = Y_e0_CMS[i] - Y_e0_ATLAS[i];
+  delta[1] = Y_e0_CMS[i] - Y_e0_NoUE[i];
+
+  Y_Delta_e0_do[i] =        *std::max_element(delta,delta+2);
+  Y_Delta_e0_up[i] = -1. * (*std::min_element(delta,delta+2));
+
+  Y_Delta_e0_overE_up[i] = Y_Delta_e0_up[i] / Y_e0_CMS[i];
+  Y_Delta_e0_overE_do[i] = Y_Delta_e0_do[i] / Y_e0_CMS[i];
+
+  Y_One[i]  = 1.;
+  X_Zero[i] = 0.;
+
+  std::cout << " :: eff[" << s1.Data() << "] = " << Y_e0_CMS[i] << std::endl;
+ }
+
+
+ //--------------------------
+ TGraph* g_e0_CMS   = new TGraph(n,X,Y_e0_CMS);
+ TGraph* g_e0_ATLAS = new TGraph(n,X,Y_e0_ATLAS);
+ TGraph* g_e0_NoUE  = new TGraph(n,X,Y_e0_NoUE);
+
+ g_e0_CMS->SetMarkerSize(1);
+ g_e0_CMS->SetMarkerStyle(20);
+ g_e0_CMS->SetMarkerColor(kRed);
+ g_e0_CMS->GetXaxis()->SetTitle("jet p_{T} threshold [GeV]");
+
+ g_e0_ATLAS->SetMarkerSize(1);
+ g_e0_ATLAS->SetMarkerStyle(21);
+ g_e0_ATLAS->SetMarkerColor(kBlue);
+ g_e0_ATLAS->GetXaxis()->SetTitle("jet p_{T} threshold [GeV]");
+
+ g_e0_NoUE->SetMarkerSize(1);
+ g_e0_NoUE->SetMarkerStyle(22);
+ g_e0_NoUE->SetMarkerColor(kGreen);
+ g_e0_NoUE->GetXaxis()->SetTitle("jet p_{T} threshold [GeV]");
+
+ TCanvas* ce0 = new TCanvas ("ce0","ce0",800,600);
+
+ TMultiGraph *mg = new TMultiGraph();
+ mg->Add(g_e0_CMS,"lp");
+ mg->Add(g_e0_ATLAS,"lp");
+ mg->Add(g_e0_NoUE,"lp");
+ mg->Draw("a");
+
+
+
+ TGraphAsymmErrors* grErr =  new TGraphAsymmErrors(n, X, Y_One, X_Zero, X_Zero, Y_Delta_e0_overE_do, Y_Delta_e0_overE_up);
+ TCanvas* ce0relative = new TCanvas ("ce0relative","ce0relative",800,600);
+ grErr->SetMarkerSize(1);
+ grErr->SetMarkerStyle(21);
+ grErr->SetMarkerColor(kRed);
+ grErr->GetXaxis()->SetTitle("jet p_{T} threshold [GeV]");
+
+ grErr->Draw ("apl");
+
+
+}
+
+
+
+
